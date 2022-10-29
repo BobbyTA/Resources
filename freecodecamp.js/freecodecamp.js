2d05ba8820ab41454aa865fd80b2849344471d3a
Bobby Atkins <bobby.timothy.atkins@gmail.com>
2022-10-28 23:19:54 -0500

    exercises focusedon objects and assigning values

--- freecodecamp.js/freecodecamp.js
+++ freecodecamp.js/freecodecamp.js
@@ -263,3 +263,74 @@ function cc(card) {//created function named cc(cardcounting) with the input of c
 cc(2); cc(3); cc(7); cc('K'); cc('A');
 
 
+const myDog = {
+  // Only change code below this line
+  name:"scoobs",
+  legs:4,
+  tails:1,
+  friends: [""]
+
+  // Only change code above this line
+};
+//created an object with multiple properties
+
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+
+//example of dot notation in my test
+
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line
+
+//example of using brackets instead of dot notation
+
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+
+//example of accessing an objects properties by way of using variables
+
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog.name = "Happy Coder";
+//updating and objects properties outside of an function
+
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = "ruff-ruff";
+//adding new properties to an javascript object
