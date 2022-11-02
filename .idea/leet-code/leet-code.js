a32ad1cd9c2055509492dd64deba0fe4e13a5a7b
Bobby Atkins <bobby.timothy.atkins@gmail.com>
2022-11-01 20:20:27 -0500

    accessing a nesting array

--- .idea/leet-code/leet-code.js
+++ .idea/leet-code/leet-code.js
@@ -48,3 +48,26 @@ const myStorage = {
     }
 };
 var gloveBoxContents = myStorage.car.inside["glove box"];
+
+
+
+const myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+const secondTree = myPlants[1].list[1];;//accessing a nesting array
