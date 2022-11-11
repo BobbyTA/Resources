3f1121f7190093d1d3010fc205a79a151fc4d2db
Bobby Atkins <bobby.timothy.atkins@gmail.com>
2022-11-10 20:28:34 -0600

    replaced loop in recursion

--- freecodecamp.js/freecodecamp.js
+++ freecodecamp.js/freecodecamp.js
@@ -409,3 +409,15 @@ do {
   i++;
 } while (i <= 10);
 
+//replacing loops while in recursion
+function sum(arr, n) {
+  // Only change code below this line
+  if(n <= 0) {
+    return 0;
+  } else {
+    return sum(arr, n - 1) + arr[n - 1];
+  }
+
+
+  // Only change code above this line
+}
