6dd6a745c73c8a4fef99c10e74b4d9e4d79a72e9
Bobby Atkins <bobby.timothy.atkins@gmail.com>
2022-11-13 21:34:08 -0600

    multiple conditional operators

--- freecodecamp.js/freecodecamp.js
+++ freecodecamp.js/freecodecamp.js
@@ -519,3 +519,16 @@ function checkEqual(a, b) {
         : num < 0 ? "negative"
             : "zero";
   }
+
+  //Use Multiple Conditional (Ternary) Operators
+// Only change code below this line
+  function countdown(n) {
+    if (n < 1) {
+      return [];
+    } else {
+      const arr = countdown(n - 1);
+      arr.unshift(n);
+      return arr;
+    }
+  }
+// Only change code above this line
