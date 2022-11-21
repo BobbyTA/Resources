cd045e198323fd07303e1fe7b0c51ecf5500fb4c
Bobby Atkins <bobby.timothy.atkins@gmail.com>
2022-11-20 19:50:14 -0600

    prevent object mutation

--- freecodecamp.js/freecodecamp.js
+++ freecodecamp.js/freecodecamp.js
@@ -564,3 +564,20 @@ function checkEqual(a, b) {
     s[2] = 7;
   }
   editInPlace();
+
+
+  function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    // Only change code below this line
+    Object.freeze(MATH_CONSTANTS);
+    // Only change code above this line
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
