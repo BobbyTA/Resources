b2cd0bed1d1397b06e3a86c2ab89aa6d9c589262
Bobby Atkins <bobby.timothy.atkins@gmail.com>
2022-11-22 20:01:17 -0600

    arrow functions with parameters

--- freecodecamp.js/freecodecamp.js
+++ freecodecamp.js/freecodecamp.js
@@ -590,3 +590,11 @@ function checkEqual(a, b) {
   const magic = () => {
     return new Date();
   };
+
+  //arrow functions with parameters
+  const myConcat = (arr1, arr2) => {
+    "use strict";
+    return arr1.concat(arr2);
+  };
+// test your code
+  console.log(myConcat([1, 2], [3, 4, 5]));
